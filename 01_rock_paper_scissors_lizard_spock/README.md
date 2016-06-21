572d5a1ba729168e08519984af9a251373ded3e8
Zoltan <znalbert@gmail.com>
2016-06-21 18:26:40 -0500

    feat: Add Rock-Paper-Scissors-Lizard-Spock
    Consolidating and polishing my previous github projects for "An Introduction to Interactive Programming in Python".  This commit adds the first project, "Rock-Paper-Scissors-Lizard-Spock" and it's README.md file.

---
+++ 01_rock_paper_scissors_lizard_spock/README.md
@@ -0,0 +1,5 @@
+# Rock-Paper-Scissors-Lizard-Spock
+
+This was my Rock-Paper-Scissors-Lizard-Spock submission for Coursera's Intro to Interactive Programming in Python course. It's a pretty simple program that models the game Rock-Paper-Scissors-Lizard-Spock, which is essentially Rock-Paper-Scissors but with two more options in order to make the game more interesting. In this version the player selects one of the five choices and the computer randomly selects a choice, after which the game prints out the results.
+
+Currently, this code is intended for use with their [Codeskulptor](http://www.codeskulptor.org/) Python interpreter, and you can play my version [here](http://www.codeskulptor.org/#user41_bR8hsxFWu0_14.py). You just have to hit the "play" icon in the upper left corner.  I may try to convert this project to work with tkinter or JavaScript if I find the time.
---
+++ 01_rock_paper_scissors_lizard_spock/rpsls.py
@@ -0,0 +1,104 @@
+# Rock-paper-scissors-lizard-Spock template
+
+
+# The key idea of this program is to equate the strings
+# "rock", "paper", "scissors", "lizard", "Spock" to numbers
+# as follows:
+#
+# 0 - rock
+# 1 - Spock
+# 2 - paper
+# 3 - lizard
+# 4 - scissors
+
+# helper functions
+
+import random
+
+def valid_choice(name):
+    """(str) -> bool
+    Checks to see if the player choice is valid.
+    """
+    return name in ["rock", "paper", "scissors", "lizard", "Spock"]
+
+def name_to_number(name):
+    """(str) -> int
+    
+    Takes a string of either rock, Spock, paper, lizard, or scissors,
+    and converts it to a corresponding number ranging from 0 - 4.
+    """
+    if name == "rock":
+        number = 0
+    elif name == "Spock":
+        number = 1
+    elif name == "paper":
+        number = 2
+    elif name == "lizard":
+        number = 3
+    elif name == "scissors":
+        number = 4
+    return number
+
+
+def number_to_name(number):
+    """(int) -> str
+    
+    Takes an integer ranging from 0 to 4 and converts it to the corresponding
+    string of either rock, paper, scissors, lizard, or Spock.
+    """
+    if number == 0:
+        name = "rock"
+    elif number == 1:
+        name = "Spock"
+    elif number == 2:
+        name = "paper"
+    elif number == 3:
+        name = "lizard"
+    elif number == 4:
+        name = "scissors"
+    return name
+
+
+def rpsls(player_choice): 
+    """(str) -> Three strings and a blank line
+    
+    Takes a string representing the player choice, randomly generates a computer
+    choice, and prints out information about the choices and winner for the round.
+    """
+    
+    # print out the message for the player's choice
+    if valid_choice(player_choice):
+        print "Player chooses " + player_choice
+    else:
+        print "Hold up there, hombre. Give a valid choice if you want to play.\n"
+        return
+
+    # convert the player's choice to player_number using the function name_to_number()
+    player_number = name_to_number(player_choice)
+    
+    # compute random guess for comp_number using random.randrange()
+    comp_number = random.randrange(0,5)
+
+    # convert comp_number to comp_choice using the function number_to_name()
+    comp_choice = number_to_name(comp_number)
+    
+    # print out the message for computer's choice
+    print "Computer chooses " + comp_choice
+
+    # compute difference of comp_number and player_number modulo five
+    winning_numb = (comp_number - player_number) % 5
+
+    # use if/elif/else to determine winner, print winner message
+    if winning_numb == 0:
+        print "Player and computer tie!\n"
+    elif winning_numb == 1 or winning_numb == 2:
+        print "Computer wins!\n"
+    else:
+        print "Player wins!\n"
+    
+# test your code - THESE CALLS MUST BE PRESENT IN YOUR SUBMITTED CODE
+rpsls("rock")
+rpsls("Spock")
+rpsls("paper")
+rpsls("lizard")
+rpsls("scissors")
